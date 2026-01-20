1f6b10340363dff01ffc006dce71201f774947c2
ines.mouine <ines.mouine@gmail.com>
2026-01-20 17:00:42 +0000

    Merge UI changes

--- Frontend/app/routes.ts
+++ Frontend/app/routes.ts
@@ -1,3 +1,10 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route, layout } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  layout("components/layout.tsx", [
+    index("pages/Dashboard.tsx"),
+    route("upload", "pages/upload.tsx"),
+    route("chatbot", "pages/chatbot.tsx"),
+  ]),
+  route("*", "pages/notfound.tsx"),
+] satisfies RouteConfig;
