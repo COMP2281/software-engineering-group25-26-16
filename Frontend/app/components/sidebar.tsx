27680f895668edd05f9be578cedea69a7a43c677
ines.mouine <ines.mouine@gmail.com>
2026-01-27 17:23:54 +0000

    added icons for the dashboard, upload and the chatbot and made slight changes to the sidebar.css

--- Frontend/app/components/sidebar.tsx
+++ Frontend/app/components/sidebar.tsx
@@ -1,41 +1,41 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router';
 import '../styles/sidebar.css';
+import { LayoutDashboard, Upload, MessageCircle } from 'lucide-react';
 
 function Sidebar() {
   const location = useLocation();
-  
-  // to check if the link is active
   function isActive(path: string) {
     return location.pathname === path;
   }
-
   return (
     <div className="sidebar">
       <div className="sidebar_header">
         <h2>Granite Guardian</h2>
       </div>
-      
       <nav className="sidebar_nav">
         <Link 
           to="/" 
           className={isActive('/') ? 'sidebar_link active' : 'sidebar_link'}
         >
-          Dashboard
+          <LayoutDashboard size={18} />
+          <span> Dashboard </span>
         </Link>
         
         <Link 
           to="/upload" 
           className={isActive('/upload') ? 'sidebar_link active' : 'sidebar_link'}
         >
-          Upload
+          <Upload size={18} />
+          <span> Upload </span>
         </Link>
         
         <Link 
           to="/chatbot" 
           className={isActive('/chatbot') ? 'sidebar_link active' : 'sidebar_link'}
         >
-          Chatbot
+          <MessageCircle size={18} />
+          <span> Chatbot </span>
         </Link>
       </nav>
     </div>
