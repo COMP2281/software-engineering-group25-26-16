5a0cd402a5b162202f0fb1145f3abfecf55953d7
Adam Fallon <adam.eric.fallon@gmail.com>
2026-03-04 18:06:53 +0000

    Improve windowing.

--- Backend/anomaly-detection/anomaly-detection.py
+++ Backend/anomaly-detection/anomaly-detection.py
@@ -1,25 +1,30 @@
 import sklearn as sk
 import pandas as pd
+import numpy as np
 
 UPLOADED_FOLDER = "../uploaded_data"
 
 def sliding_window(df: pd.DataFrame, n=5):
-    window_pairs = []
+    size = len(df) - n
+    window_inputs = {}
+    window_targets = {}
 
     # iterate over the columns as series
-    for col in df.items():
-        for i in range(len(col) - n):
-            # pair up the first n values, and the value after them
-            window_pairs.append((col[i:i+n], col[i+n]))
+    for col in df.columns:
+        series = df[col].to_numpy()
+        window_inputs[col] = np.array([series[i:i+n] for i in range(size)])
+        window_targets[col] = series[n:]
 
-    return window_pairs
+    assert len(window_inputs) == len(window_targets)
+
+    return window_inputs, window_targets
 
 
 def feature_selection(df):
     cols = ["ENGINE LOAD", "ENGINE RPM", "LONG TERM FUEL TRIM BANK 1", "FUEL TANK", "INTAKE MANIFOLD PRESSURE", "CATALYST TEMPERATURE BANK1 SENSOR1", "CATALYST TEMPERATURE BANK1 SENSOR2", "COOLANT TEMPERATURE", "COOLANT TEMPERATURE STD"]
     return df[cols]
 
-def clean_data(df, n=5) -> pd.DataFrame:
+def clean_data(df, n=5):
     # add standard deviation of coolant temperature as a feature
     std_temp = df["COOLANT TEMPERATURE"].rolling(n).std()
 
@@ -28,19 +33,17 @@ def clean_data(df, n=5) -> pd.DataFrame:
     std_temp_max = std_temp.max()
     df["COOLANT TEMPERATURE STD"] = (std_temp - std_temp_min) / (std_temp_max - std_temp_min)
 
-    # perform feature extraction
-    window_pairs = sliding_window(df, n=5)
-
     # perform feature selection
-    feature_selection(df)
+    df = feature_selection(df)
+
+    # perform feature extraction
+    window_inputs, window_targets = sliding_window(df, n=5)
 
     # save to file
     path = "cleaned_drive1.csv"
     df.to_csv(path, index=False)
 
-    return df
-
-    
+    return window_inputs, window_targets
 
 
 
@@ -55,15 +58,18 @@ if __name__ == "__main__":
     ratio_anomaly = 0.3
 
     num_anomalous = int(num_files * ratio_anomaly)
+    df = load_data_frame(f"../sample_data/idle1.csv")
 
     # anomalous data
     for file in range(1, num_anomalous + 1):
+        print(f"Processing file {file}...")
         df = load_data_frame(f"../sample_data/idle{file}.csv")
         clean_data(df)
         anomalous.append(df)
 
     # normal data
     for file in range(num_anomalous, num_files + 1):
+        print(f"Processing file {file}...")
         df = load_data_frame(f"../sample_data/idle{file}.csv")
         clean_data(df)
         normal.append(df)
