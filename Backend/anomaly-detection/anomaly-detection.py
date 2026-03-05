888fee36f421e4056b16d4f2bc8808c53e98dade
Adam Fallon <adam.eric.fallon@gmail.com>
2026-03-05 17:36:47 +0000

    Add KNN classifier (Adam's implementation).

--- Backend/anomaly-detection/anomaly-detection.py
+++ Backend/anomaly-detection/anomaly-detection.py
@@ -1,6 +1,8 @@
-import sklearn as sk
 import pandas as pd
 import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from sklearn import svm
 
 UPLOADED_FOLDER = "../uploaded_data"
 
@@ -20,8 +22,12 @@ def sliding_window(df: pd.DataFrame, n=5):
     return window_inputs, window_targets
 
 def create_sliding_windows(df, window_size):
-    windows = [df.iloc[i:i+window_size].values for i in range(len(df) - window_size + 1)]
-    return np.array(windows)
+    windows = [df.iloc[i:i+window_size].values.flatten() for i in range(len(df) - window_size + 1)]
+    windows = np.array(windows)
+
+    # remove any rows that contain NaN values
+    windows = windows[~np.isnan(windows).any(axis=1)]
+    return windows
 
 def feature_selection(df):
     cols = ["ENGINE LOAD", "ENGINE RPM", "LONG TERM FUEL TRIM BANK 1", "FUEL TANK", "INTAKE MANIFOLD PRESSURE", "CATALYST TEMPERATURE BANK1 SENSOR1", "CATALYST TEMPERATURE BANK1 SENSOR2", "COOLANT TEMPERATURE", "COOLANT TEMPERATURE STD"]
@@ -40,7 +46,7 @@ def clean_data(df, window_size=5):
     df = feature_selection(df)
 
     # perform feature extraction
-    #window_inputs, window_targets = sliding_window(df, n=5)
+    # will also flatten each window
     windows = create_sliding_windows(df, window_size=window_size)
 
     # # save to file
@@ -48,7 +54,7 @@ def clean_data(df, window_size=5):
     # df.to_csv(path, index=False)
 
     # shape of windows = (num_windows, num_rows_per_window, num_features)
-    print(f"Shape: {windows.shape}");
+
 
     return windows
 
@@ -105,27 +111,74 @@ def add_noise_for_engine_coolant_temperature(df, snr_db=20, alpha=1.0, random_st
 def load_data_frame(path) -> pd.DataFrame:
     return pd.read_csv(path, index_col=False)
 
+def train_knn(anomalous, normal):
+    train_data = np.array(anomalous + normal)
+    print(f"Training KNN with {len(anomalous)} anomalous samples and {len(normal)} normal samples ({len(train_data)} total)...")
+
+    model = svm.SVC()
+    model.fit(train_data, np.array([True] * len(anomalous) + [False] * len(normal)))
+
+    # model = KNeighborsClassifier()
+    # model.fit(train_data, np.array([True] * len(anomalous) + [False] * len(normal)))
+    return model
+
+def test_knn(knn, anomalous, normal):
+    test_data = np.array(anomalous + normal)
+    test_labels = np.array([True] * len(anomalous) + [False] * len(normal))
+
+    print(f"Testing KNN with {len(anomalous)} anomalous samples and {len(normal)} normal samples ({len(test_data)} total)...")
+
+    accuracy = accuracy_score(test_labels, knn.predict(test_data))
+    print(f"KNN Accuracy: {accuracy:.4f}")
+
+def process_file(file, anomalous):
+    print(f"Processing file {file}...")
+    windows = load_data_frame(file)
+    if anomalous:
+        windows = add_noise_for_engine_coolant_temperature(windows, snr_db=40, alpha=1.0, random_state=28)
+    windows = clean_data(windows)
+    return windows
+
 if __name__ == "__main__":
-    normal = []
-    anomalous = []
+    normal_data = []
+    anomalous_data = []
 
-    num_files = 47
-    ratio_anomaly = 0.3
+    num_files_idle = 47
+    num_files_drive = 13
+    ratio_anomaly = 0.5
 
-    num_anomalous = int(num_files * ratio_anomaly)
-    df = load_data_frame(f"../sample_data/idle1.csv")
+    ratio_test = 0.2
 
-    # anomalous data
-    for file in range(1, num_anomalous + 1):
-        print(f"Processing file {file}...")
-        df = load_data_frame(f"../sample_data/idle{file}.csv")
-        df = add_noise_for_engine_coolant_temperature(df, snr_db=20, alpha=1.0, random_state=42)
-        df = clean_data(df)
-        anomalous.append(df)
+    num_anomalous = int(num_files_idle * ratio_anomaly)
+    windows = load_data_frame(f"../sample_data/idle1.csv")
 
-    # normal data
-    for file in range(num_anomalous, num_files + 1):
-        print(f"Processing file {file}...")
-        df = load_data_frame(f"../sample_data/idle{file}.csv")
-        df = clean_data(df)
-        normal.append(df)
+    # anomalous data (idle)
+    for file in range(1, int(num_files_idle * ratio_anomaly) + 1):
+        anomalous_data.extend(process_file(f"../sample_data/idle{file}.csv", anomalous=True))
+
+    # normal data (idle)
+    for file in range(int(num_files_idle * ratio_anomaly), num_files_idle + 1):
+        normal_data.extend(process_file(f"../sample_data/idle{file}.csv", anomalous=False))
+
+
+    # anomalous data (drive)
+    for file in range(1, int(num_files_drive * ratio_anomaly) + 1):
+        anomalous_data.extend(process_file(f"../sample_data/drive{file}.csv", anomalous=True))
+
+    # normal data (drive)
+    for file in range(int(num_files_drive * ratio_anomaly), num_files_drive + 1):
+        normal_data.extend(process_file(f"../sample_data/drive{file}.csv", anomalous=False))
+
+    anomalous_train = anomalous_data[:int(len(anomalous_data) * (1 - ratio_test))]
+    anomalous_test = anomalous_data[int(len(anomalous_data) * (1 - ratio_test)):]
+    normal_train = normal_data[:int(len(normal_data) * (1 - ratio_test))]
+    normal_test = normal_data[int(len(normal_data) * (1 - ratio_test)):]
+
+    # show sizes
+    print(f"Anomalous train samples: {len(anomalous_train)}")
+    print(f"Anomalous test samples: {len(anomalous_test)}")
+    print(f"Normal train samples: {len(normal_train)}")
+    print(f"Normal test samples: {len(normal_test)}")
+
+    knn = train_knn(anomalous_train, normal_train)
+    test_knn(knn, anomalous_test, normal_test)
