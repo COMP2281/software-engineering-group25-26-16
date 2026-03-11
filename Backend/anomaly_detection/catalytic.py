a7f0b4fcd77a948df0e017f2bf71166d259687c7
Adam Fallon <adam.eric.fallon@gmail.com>
2026-03-11 14:01:59 +0000

    Test/fix catalytic temperature warnings.

--- Backend/anomaly_detection/catalytic.py
+++ Backend/anomaly_detection/catalytic.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
 
-from .base_warning import Severity
-from anomaly_detection.base_warning import BaseWarning
+from base_warning import Severity
+from base_warning import BaseWarning
 
 
 # the amount that the outlet must be hotter than the inlet after TIME_THRESHOLD seconds have elapsed.
@@ -22,16 +22,22 @@ class CatalyticClassifier():
         time_thresholding = df["ENGINE RUN TIME"] > TIME_THRESHOLD
 
         run_time = run_time[time_thresholding]
+        
+        if len(run_time) == 0:
+            return warnings
+
+
         inlet_temp = df["CATALYST TEMPERATURE BANK1 SENSOR2"].values[time_thresholding]
         outlet_temp = df["CATALYST TEMPERATURE BANK1 SENSOR1"].values[time_thresholding]
 
         anomaly_condition = (outlet_temp - inlet_temp) < DIFFERENCE_THRESHOLD
         
-        # anomaly should be detected 5 times in a row before we raise a
-        # warning, to avoid false positives from random fluctuations
-        anomalies = np.convolve(anomaly_condition, np.ones(5, dtype=int), mode='valid') == 5
+        size_block = 5
+        # anomaly should be detected `size_block` times in a row before we
+        # raise a warning, to avoid false positives from sensor noise
+        anomalies = np.convolve(anomaly_condition, np.ones(size_block, dtype=int), mode='valid') == size_block
 
-        for run_time in run_time[anomalies]:
+        for run_time in run_time[:-size_block + 1][anomalies]:
             warnings.append(CatalyticWarning(run_time))
 
         return warnings
@@ -44,3 +50,10 @@ class CatalyticWarning(BaseWarning):
 
     def message(self) -> str:
         return "Catalytic converter is not producing enough heat - might be malfunctioning."
+
+# for testing purposes only
+# if __name__ == "__main__":
+#     model = CatalyticClassifier()
+#     warnings = model.generate_warnings(f"../sample_data/drive1test.csv")
+#     for warning in warnings:
+#         print(warning.to_dict())
