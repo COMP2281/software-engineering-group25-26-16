fa682b6014f1df4a8daaf931147d14c0b9d55391
Adam Fallon <adam.eric.fallon@gmail.com>
2026-03-12 03:32:05 +0000

    Add test cases for catalytic classifier.

--- Backend/anomaly_detection/catalytic.py
+++ Backend/anomaly_detection/catalytic.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import numpy as np
-from .base_warning import Severity, BaseWarning
+
+try:
+    from .base_warning import Severity, BaseWarning
+except:
+    from base_warning import Severity, BaseWarning
 
 
 # the amount that the outlet must be hotter than the inlet after TIME_THRESHOLD seconds
@@ -88,7 +92,9 @@ class CatalyticClassifier():
 
     def generate_warnings(self, filepath) -> list[BaseWarning]:
         df = pd.read_csv(filepath, index_col=False)
+        return self.generate_warnings_from_df(df)
 
+    def generate_warnings_from_df(self, df) -> list[BaseWarning]:
         warnings = []
         warnings.extend(self.generate_warnings_diff(df))
         warnings.extend(self.generate_too_hot_warnings(df))
@@ -110,7 +116,6 @@ class CatalyticWarning(BaseWarning):
 
 
 class CatalyticTooHotWarning(BaseWarning):
-
     def __init__(self, run_time: float) -> None:
         super().__init__(run_time, severity=Severity.HIGH)
 
@@ -120,3 +125,52 @@ class CatalyticTooHotWarning(BaseWarning):
             "caused by a misfiring engine or unburned fuel entering the "
             "converter. The vehicle should be inspected by a mechanic."
         )
+
+# testing
+if __name__ == "__main__":
+    classifier = CatalyticClassifier()
+    
+    df1 = pd.read_csv("../sample_data/drive1.csv", index_col=False)
+    df_orig = df1.copy()
+
+    warnings1 = classifier.generate_warnings_from_df(df1)
+
+    # (1) the data should not trigger any warnings because the outlet is hotter
+    # than the inlet after 10 minutes
+    assert len(warnings1) == 0, f"Expected no warnings, got {len(warnings1)}"
+
+
+    # (2) making the outlet colder than the inlet at the very end of the data,
+    # which should not trigger a warning because it is only one reading and
+    # could be noise
+    df1.loc[df1.index[-1], "CATALYST TEMPERATURE BANK1 SENSOR1"] = (
+        df1.loc[df1.index[-1], "CATALYST TEMPERATURE BANK1 SENSOR2"] - DIFFERENCE_THRESHOLD - 1
+    )
+
+    warnings2 = classifier.generate_warnings_from_df(df1)
+    assert len(warnings2) == 0, f"Expected no warning because of noise, got {len(warnings2)}"
+
+    # (3) making the outlet colder than the inlet for several consecutive
+    # readings, which should trigger a warning
+    size_block = 5
+    for i in range(-size_block, 0):
+        df1.loc[df1.index[i], "CATALYST TEMPERATURE BANK1 SENSOR1"] = (
+            df1.loc[df1.index[i], "CATALYST TEMPERATURE BANK1 SENSOR2"] - DIFFERENCE_THRESHOLD - 1
+        )
+
+    warnings3 = classifier.generate_warnings_from_df(df1)
+    assert len(warnings3) > 0, f"Expected warnings, got {len(warnings3)}"
+
+    # (4) make the outlet extremely hot which should trigger a warning
+    df_orig.loc[10, "CATALYST TEMPERATURE BANK1 SENSOR1"] = TOO_HOT_THRESHOLD + 10
+    warnings4 = classifier.generate_warnings_from_df(df_orig)
+    assert len(warnings4) == 0, f"Expected no warnings because of noise, got {len(warnings4)}"
+
+    # (5) make the outlet extremely hot for several consecutive readings, which should trigger a warning
+    for i in range(10, 10 + size_block):
+        df_orig.loc[i, "CATALYST TEMPERATURE BANK1 SENSOR1"] = TOO_HOT_THRESHOLD + 10
+
+    warnings5 = classifier.generate_warnings_from_df(df_orig)
+    assert len(warnings5) > 0, f"Expected warnings, got {len(warnings5)}"
+
+    print("Tests passed")
