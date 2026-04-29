8c856933431e6e6954c17578df52028fd9514ff6
Adam Eric Fallon <adam.eric.fallon@gmail.com>
2026-04-29 21:09:40 +0100

    Add docker compose file that grabs from ghcr.io.

--- docker/run_ollama.sh
+++ docker/run_ollama.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
 echo "Starting Ollama server..."
-ollama serve &  # Start Ollama in the background
-
-# echo "Ollama is pulling the model..."
-# ollama pull $GRANITE_MODEL
-
-echo "Ollama is ready!"
-wait
+ollama serve
+echo "Exiting Ollama server..."
